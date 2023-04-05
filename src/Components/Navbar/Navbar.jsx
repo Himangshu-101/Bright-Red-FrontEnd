5d07a6ab6045b36801cb3d53e48e7e9be5e0bf5c
Amarjeet-27 <ajchaurasia1214@gmail.com>
2023-04-05 20:11:18 +0530

    "ADD: Navbar"

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,17 @@
+import styles from "./Navbar.module.scss";
+
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.title}>
+        Trash<span>T</span>rade
+      </div>
+      <div className={styles.right}>
+        <button>Login</button>
+        <i className="fa-sharp fa-solid fa-cart-plus"></i>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
